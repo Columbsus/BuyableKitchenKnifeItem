462ddfabf43acc703e4f199a3ea4cabafc7c1594
Columbus <columbuzalphamale@gmail.com>
2024-04-05 16:25:34 -0700

    Add project files.

---
+++ BuyableKnife/Main.cs
@@ -0,0 +1,158 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using LethalLib.Modules;
+using UnityEngine.SceneManagement;
+using BepInEx.Configuration;
+using Dissonance;
+using static UnityEngine.UI.Image;
+using Unity.Netcode;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+using Steamworks.Ugc;
+using NetworkPrefabs = LethalLib.Modules.NetworkPrefabs;
+
+namespace BuyableKnife
+{
+    [BepInDependency("evaisa.lethallib", "0.13.2")]
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BuyableKnife : BaseUnityPlugin
+    {
+        private const string modGUID = "Columbus.BuyableKnife";
+        private const string modName = "Buyable Knife";
+        private const string modVersion = "1.1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static BuyableKnife Instance;
+
+        private static ManualLogSource LoggerInstance => Instance.Logger;
+
+        public static List<Item> AllItems => Resources.FindObjectsOfTypeAll<Item>().Concat(UnityEngine.Object.FindObjectsByType<Item>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID)).ToList();
+        public static Item Knife => AllItems.FirstOrDefault(item => item.name.Equals("Knife"));
+        public static Item KnifeClone { get; private set; }
+        public static GameObject KnifeObjectClone { get; private set; }
+
+        private static ConfigEntry<int> KnifePriceConfig;
+        public static int KnifePrice => KnifePriceConfig.Value;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(this);
+                Instance = this;
+            }
+            harmony.PatchAll();
+            KnifePriceConfig = Config.Bind("Prices", "KnifePrice", 25, "Credits needed to buy Knife");
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            KnifeClone = MakeNonScrap(KnifePrice);
+            AddToShop();
+            Logger.LogInfo($"Plugin {modName} is loaded with version {modVersion}!");
+        }
+
+        private static Item MakeNonScrap(int price)
+        {
+            Item nonScrap = ScriptableObject.CreateInstance<Item>();
+            DontDestroyOnLoad(nonScrap);
+            nonScrap.name = "Error";
+            nonScrap.itemName = "Error";
+            nonScrap.itemId = 6624;
+            nonScrap.isScrap = false;
+            nonScrap.creditsWorth = price;
+            nonScrap.canBeGrabbedBeforeGameStart = true;
+            nonScrap.automaticallySetUsingPower = false;
+            nonScrap.batteryUsage = 300;
+            nonScrap.canBeInspected = false;
+            nonScrap.isDefensiveWeapon = true;
+            nonScrap.saveItemVariable = true;
+            nonScrap.syncGrabFunction = false;
+            nonScrap.twoHandedAnimation = true;
+            nonScrap.verticalOffset = 0.25f;
+            var prefab = LethalLib.Modules.NetworkPrefabs.CreateNetworkPrefab("Cube");
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.SetParent(prefab.transform, false);
+            cube.GetComponent<MeshRenderer>().sharedMaterial.shader = Shader.Find("HDRP/Lit");
+            prefab.AddComponent<BoxCollider>().size = Vector3.one * 2;
+            prefab.AddComponent<AudioSource>();
+            var prop = prefab.AddComponent<PhysicsProp>();
+            prop.itemProperties = nonScrap;
+            prop.grabbable = true;
+            nonScrap.spawnPrefab = prefab;
+            prefab.tag = "PhysicsProp";
+            prefab.layer = LayerMask.NameToLayer("Props");
+            cube.layer = LayerMask.NameToLayer("Props");
+            try
+            {
+                GameObject scanNode = GameObject.Instantiate<GameObject>(Items.scanNodePrefab, prefab.transform);
+                scanNode.name = "ScanNode";
+                scanNode.transform.localPosition = new Vector3(0f, 0f, 0f);
+                scanNode.transform.localScale *= 2;
+                ScanNodeProperties properties = scanNode.GetComponent<ScanNodeProperties>();
+                properties.nodeType = 1;
+                properties.headerText = "Error";
+                properties.subText = $"A mod is incompatible with {modName}";
+            }
+            catch (Exception e)
+            {
+                LoggerInstance.LogError(e.ToString());
+            }
+            prefab.transform.localScale = Vector3.one / 2;
+            return nonScrap;
+        }
+
+        private static GameObject CloneNonScrap(Item original, Item clone, int price)
+        {
+            var prefab = NetworkPrefabs.CloneNetworkPrefab(original.spawnPrefab);
+            DontDestroyOnLoad(prefab);
+            CopyFields(original, clone);
+            prefab.GetComponent<GrabbableObject>().itemProperties = clone;
+            clone.spawnPrefab = prefab;
+            clone.name = "Buyable" + original.name;
+            clone.creditsWorth = price;
+            return prefab;
+        }
+
+        public static void CopyFields(Item source, Item destination)
+        {
+            FieldInfo[] fields = typeof(Item).GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                field.SetValue(destination, field.GetValue(source));
+            }
+        }
+
+        private static Dictionary<string, TerminalNode> infoNodes = new Dictionary<string, TerminalNode>();
+
+        private static TerminalNode CreateInfoNode(string name, string description)
+        {
+            if (infoNodes.ContainsKey(name)) return infoNodes[name];
+            TerminalNode node = ScriptableObject.CreateInstance<TerminalNode>();
+            DontDestroyOnLoad(node);
+            node.clearPreviousText = true;
+            node.name = name + "InfoNode";
+            node.displayText = description + "\n\n";
+            infoNodes.Add(name, node);
+            return node;
+        }
+
+        private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            LoggerInstance.LogInfo("Scene \"" + scene.name + "\" loaded with " + mode + " mode.");
+            if (Knife == null) return;
+            if (KnifeObjectClone != null) return;
+            KnifeObjectClone = CloneNonScrap(Knife, KnifeClone, KnifePrice);
+        }
+
+        private static void AddToShop()
+        {
+            Items.RegisterShopItem(KnifeClone, price: KnifePrice, itemInfo: CreateInfoNode("Knife", "Kitchen Knife. Can be used to stab enemies or your teammates for the hell of it."));
+            LoggerInstance.LogInfo($"Knife added to Shop for {KnifePrice} credits");
+        }
+    }
+}
